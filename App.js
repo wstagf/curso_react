42618c79aafabd8625013e9dd144db5df6a14148
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-01-12 23:02:32 -0300

    Configurado build

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text>Hello World</Text>
+        <Text>Hello World2</Text>
       </SafeAreaView>
     </>
   );
